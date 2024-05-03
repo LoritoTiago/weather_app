d84e36c865f75b2d01edec957df9df0e19414413
Lorito Tiago <58330997+LoritoTiago@users.noreply.github.com>
2024-05-03 11:01:58 +0100

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Lorito Mbuta on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
