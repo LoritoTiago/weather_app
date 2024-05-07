5ccad7d7271fa4724f72732555fd9b516fdf8f5b
Lorito Tiago <58330997+LoritoTiago@users.noreply.github.com>
2024-05-07 13:00:39 +0100

    Feat(home): add dynamic current item

---
+++ weather/Model/MockData.swift
@@ -0,0 +1,15 @@
+//
+//  MockData.swift
+//  weather
+//
+//  Created by Lorito Mbuta on 07/05/2024.
+//
+
+import Foundation
+
+
+struct MockData {
+   
+}
+
+
---
+++ weather/Model/Weather.swift
@@ -0,0 +1,8 @@
+//
+//  Weather.swift
+//  weather
+//
+//  Created by Lorito Mbuta on 07/05/2024.
+//
+
+import Foundation
